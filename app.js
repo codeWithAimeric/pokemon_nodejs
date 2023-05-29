0a705e6ad2412e6695e5f4d51cd464c5a2801c7b
Aimeric <aimericjjb@gmail.com>
2023-05-29 13:29:10 +0300

    add helper for response message and structure the response of the api in json and add a message of success

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { success } = require('./helper');
 let pokemons = require('./mock-pokemon');
 
 const app = express();
@@ -9,12 +10,13 @@ app.get('/', (req, res) => res.send('Hello, Express !!'));
 app.get('/api/pokemons/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const pokemon = pokemons.find(pokemon =>  pokemon.id === id);
-    res.send(`Vous avez demandé le pokemon : ${pokemon.name}`); 
+    const message = 'Un pokémon a bien été trouvé.';
+    res.json(success(message, pokemon));
 });
 
 app.get('/api/pokemons', (req, res) => {
-    const numberPokemons = pokemons.length; 
-    res.send(`Il y a ${numberPokemons} dans le pokedex pour le moment`);
+    const message = 'Voici la liste complète de tout les pokémons';
+    res.json(success(message, pokemons));
 });
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`));
